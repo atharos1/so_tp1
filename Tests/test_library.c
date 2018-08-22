cb6544a06063e0799f171c7fa8edc0ca266f0e9e
agusosimani <aosimani@itba.edu.ar>
2018-08-22 14:11:52 -0300

    funciones que no usamos

--- Tests/test_library.c
+++ Tests/test_library.c
@@ -8,18 +8,6 @@ void check_is_not_null(void * pointer) {
     }
 }
 
-void check_strings_equal(char * s1, char * s2) {
-    if (strcmp(s1, s2) != 0) {
-        failed("Expected: two equal strings.\nReceived: two different strings.");
-    } else {
-        passed();
-    }
-}
-
-void then_success() {
-    passed();
-}
-
 void passed() {
     printf("\tPassed!\n");
 }
@@ -27,10 +15,4 @@ void passed() {
 void failed(char * error) {
     printf("\tFailed\n");
     fprintf(stderr, "%s", error);
-}
-
-void not_implemented() {
-    fflush(stdout);
-    fprintf(stderr, "Not implemented");
-    exit(1);
 }
